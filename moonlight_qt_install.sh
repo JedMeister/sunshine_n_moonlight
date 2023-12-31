3deb5f26b3bf7f6a89340f63a06432e09d914927
Jeremy Davis <jeremy@turnkeylinux.org>
2024-01-01 10:15:06 +1100

    Only run as root (via sudo)

--- moonlight_qt_install.sh
+++ moonlight_qt_install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+if [[ $EUID -ne 0 ]]; then
+	echo "Please re-run with sudo (or as root)"
+	exit 1
+fi
+
 base_url="https://dl.cloudsmith.io/public/moonlight-game-streaming/moonlight-qt"
 gpg_keyring_path="/usr/share/keyrings/moonlight-game-streaming-moonlight-qt-archive-keyring.gpg"
 gpg_url="$base_url/gpg.2F6AE14E1C660D44.key"
@@ -13,3 +18,6 @@ deb [signed-by=$gpg_keyring_path] $base_url/deb/debian bookworm main
 
 #deb-src [signed-by=$gpg_keyring_path] $base_url/deb/debian bookworm main
 EOF
+
+apt-get update
+apt-get install moonlight-qt
